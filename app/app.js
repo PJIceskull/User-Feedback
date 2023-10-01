6bebde05c863b18dee3f36825a8cd8302c89d9f8
Pierce Issah <PJIceskull2003@gmail.com>
2023-10-01 18:17:59 -0400

    change file dir & created more html files

--- app/app.js
+++ app/app.js
@@ -1,26 +1,31 @@
-// function changeRoute() {
-//   let hashTag = window.location.hash;
-//   let pageID = hashTag.replace("#", "");
-//   //   console.log(hashTag + ' ' + pageID);
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  let path = pageID.split("/");
+  //   console.log(hashTag + ' ' + pageID);
 
-//   if (pageID != "") {
-//     $.get(`pages/pageID/pageID.html`, function (data) {
-//       console.log("data " + data);
-//       $("#app").html(data);
-//     });
-//   } else {
-//     $.get(`pages/home/home.html`, function (data) {
-//       console.log("data " + data);
-//       $("#app").html(data);
-//     });
-//   }
-// }
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      //   console.log("data " + data);
+      //   console.log(data);
+      console.log(pageID); // print ot console the pageI
+      $("#app").html(data);
+    }).fail(function () {
+      console.log("Page not found");
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  }
+}
 
 function initListeners() {}
 
 function initSite() {
   $(window).on("hashchange", changeRoute);
-  route();
+  changeRoute();
 }
 
 $(document).ready(function () {
